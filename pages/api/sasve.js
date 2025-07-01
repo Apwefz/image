ae091ef6a374124416daf3d29cbb5608ae5b3212
Apwefz <159957548+Apwefz@users.noreply.github.com>
2025-07-01 07:09:52 +0300

    Update sasve.js

--- pages/api/sasve.js
+++ pages/api/sasve.js
@@ -1,1 +1,15 @@
-{"password":"ömer"}
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'keys.json');
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({ error: "Dosya bulunamadı" });
+  }
+
+  const data = fs.readFileSync(filePath, 'utf8');
+  const keys = JSON.parse(data);
+  
+  res.status(200).json(keys);
+}
